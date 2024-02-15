2bf99596693ada1973d5452899665f81064c0e53
Miki Vanoušek <vanousekmikulas@gmail.com>
2024-02-15 18:01:34 +0100

    add copy plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -15,4 +16,11 @@ module.exports = {
       { test: /\.tsx?$/, loader: 'ts-loader' },
     ],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        { from: 'src/*.html', to: '[name].[ext]' },
+      ],
+    }),
+  ],
 };
