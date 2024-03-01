965866496f928e74af63733e85a18b21447a17b0
Miki Vanoušek <vanousekmikulas@gmail.com>
2024-03-01 16:51:05 +0100

    parsing with any symbol

--- test/pg_diagram.test.ts
+++ test/pg_diagram.test.ts
@@ -17,4 +17,7 @@ test('parsing test', () => {
     let pg = PGParser.parse_pg_format(filestr)
     expect(pg.nodes.length).toBe(6)
     expect(pg.links.length).toBe(8)
+    expect(pg.nodes[0].label).toBe("0")
+    expect(pg.nodes[1].label).toBe("1")
+    expect(pg.nodes[2].label).toBe('2 3"4')
 })
