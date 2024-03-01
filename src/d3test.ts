5631c8781727be8f14dfabb80dd62e7f8d3ee4e2
DeKleineKabouter <n.vanduijl@student.utwente.nl>
2024-03-01 16:03:21 +0100

    calculated link coordinates and drew arrows

--- src/d3test.ts
+++ src/d3test.ts
@@ -5,17 +5,17 @@ export function d3test(container: HTMLElement) {
   var height = 500;
 
   var vertices = [
-    { x: 80, y: 50 },
-    { x: 50, y: 150 },
-    { x: 250, y: 350 },
-    { x: 150, y: 50 },
+    { x: 80, y: 50, id: 1 },
+    { x: 50, y: 150, id: 2 },
+    { x: 250, y: 350, id: 3 },
+    { x: 150, y: 50, id: 4 },
   ];
 
   var edges = [
-    { source: 0, target: 1 },
+    { source: 4, target: 1 },
     { source: 1, target: 2 },
     { source: 2, target: 3 },
-    { source: 3, target: 0 },
+    { source: 3, target: 4 },
   ];
 
   var svg = d3
@@ -39,24 +39,60 @@ export function d3test(container: HTMLElement) {
     .attr("r", 25)
     .attr("fill", "#69b3a2");
 
-  let links = [];
+  svg
+    .append("svg:defs")
+    .append("svg:marker")
+    .attr("id", "triangle")
+    .attr("refX", 6)
+    .attr("refY", 6)
+    .attr("markerWidth", 30)
+    .attr("markerHeight", 30)
+    .attr("orient", "auto")
+    .append("path")
+    .attr("d", "M 0 0 12 6 0 12 3 6")
+    .style("fill", "black");
 
   for (let i = 0; i < edges.length; i++) {
-    links.push(
-      d3.linkHorizontal()({
-        source: [vertices[edges[i].source].x, vertices[edges[i].source].y],
-        target: [vertices[edges[i].target].x, vertices[edges[i].target].y],
-      })
+    var link = calculateLinkCoordinates(
+      {
+        x: vertices.find((v) => v.id === edges[i].source)!.x,
+        y: vertices.find((v) => v.id === edges[i].source)!.y,
+      },
+      {
+        x: vertices.find((v) => v.id === edges[i].target)!.x,
+        y: vertices.find((v) => v.id === edges[i].target)!.y,
+      }
     );
-  }
-
-  for (let i = 0; i < links.length; i++) {
     svg
-      .append("path")
-      .attr("d", links[i])
+      .append("line")
+      .attr("x1", link.source.x)
+      .attr("y1", link.source.y)
+      .attr("x2", link.target.x)
+      .attr("y2", link.target.y)
       .attr("stroke", "black")
-      .attr("fill", "none");
+      .attr("stroke-width", 1)
+      .attr("marker-end", "url(#triangle)");
   }
 
   container.append(svg.node()!);
+}
+
+export function calculateLinkCoordinates(
+  source: { x: number; y: number },
+  target: { x: number; y: number }
+) {
+  let dx = target.x - source.x;
+  let dy = target.y - source.y;
+  let dr = Math.sqrt(dx * dx + dy * dy);
+  let link = {
+    source: {
+      x: source.x + (dx / dr) * 25,
+      y: source.y + (dy / dr) * 25,
+    },
+    target: {
+      x: target.x - (dx / dr) * 25,
+      y: target.y - (dy / dr) * 25,
+    },
+  };
+  return link;
 }
