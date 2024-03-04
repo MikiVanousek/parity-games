9cb45e27c9fe763fa4ed5b037947452e876cb08c
HesselStokman <hesselstokman@gmail.com>
2024-03-04 15:00:43 +0100

    round and square nodes

--- src/index.ts
+++ src/index.ts
@@ -6,10 +6,30 @@ let pg = new PG.ParityGame();
 let id = 0;
 let cy = cytoscape({
     container: document.getElementById('cy'),
-    elements: []
+    elements: [],
+    style: [
+      {
+        selector: 'node[isEven = "true"]',
+        style: {
+          'shape': 'ellipse', // Round shape for even nodes
+          //'background-color': '#66CCFF', // Example color
+          // Define other styles as needed
+        }
+      },
+      {
+        selector: 'node[isEven = "false"]',
+        style: {
+          'shape': 'rectangle', // Square shape for odd nodes
+          //'background-color': '#FF6666', // Example color
+          // Define other styles as needed
+        }
+      },
+      
+    ],
   });
 let mouseX: number = 0;
 let mouseY: number = 0;
+let firstNodeId: string = null;
 
 document.addEventListener('mousemove', (event: MouseEvent) => {
   mouseX = event.clientX;
@@ -23,7 +43,6 @@ document.addEventListener('keydown', (event: KeyboardEvent) => {
 
   switch(event.key) {
     case 'e': {
-      console.log('dfdsfds')
       addNodeAtPosition(relativeX, relativeY, true)
       break;
     }
@@ -38,9 +57,48 @@ function addNodeAtPosition(x: number, y: number, isEven: boolean) {
   id = pg.addNode(0, isEven ? PG.Player.Even : PG.Player.Odd)
   cy.add({
     group: 'nodes',
-    data: { id: String(id)},
+    data: {
+      id: String(id),
+      isEven: String(isEven) // Store isEven as a string to match the selector
+    },
     position: { x: x, y: y }
   });
+  cy.resize()
 }
+
+function addEdge(nodeId: number) {
+  cy.add({
+    group: 'edges',
+    data: { source: firstNodeId, target: nodeId }
+  });
+}
+
+cy.on('tap', 'node', function(evt) {
+  const nodeId = evt.target.id();
+  if (firstNodeId === null) {
+    // If no node is selected yet, store the current node's ID
+    firstNodeId = nodeId;
+  } else if (firstNodeId !== nodeId) {
+    // If a different node is clicked, create an edge from the first node to this one
+    addEdge(nodeId)
+
+    // Reset firstNodeId for the next selection
+    firstNodeId = null;
+  }
+
+  cy.on('tap', function(event) {
+    const evtTarget = event.target;
+  
+    if (evtTarget === cy) {
+      // The tap was on the core background, not on any element
+      firstNodeId = null; // Deselect any selected node
+  
+      // Optional: remove any visual indication of selection
+      // This could be adjusting styles or classes on previously selected nodes
+    }
+  });
+  // If the same node is clicked again, you might want to reset firstNodeId or do nothing
+  // This part of logic can be adjusted based on the desired behavior
+});
 
 
