76f107c9bef2662f72204ae55fa5ade3bd02f808
Miki Vanoušek <vanousekmikulas@gmail.com>
2024-03-25 17:24:49 +0100

    move trace

--- src/index.ts
+++ src/index.ts
@@ -2,14 +2,7 @@ import { ParityGame } from './board/ParityGame';
 import { Player } from "./board/Node";
 import LayoutManager from "./layout/layoutManager";
 import { PGParser } from './board/PGParser';
-import { example_pg } from "../src/board/ExamplePG";
-import { setupTraceUI } from './TraceUI';
-import { cy, defaultLayout, layoutManager, ur } from './Cytosetup';
-
-setupTraceUI()
-let pg = example_pg;
-cy.add(pg.getElementDefinition());
-
+import { example_pg } from './board/ExamplePG';
 
 declare global {
   interface Window {
@@ -17,6 +10,13 @@ declare global {
   }
 }
 
+var pg = example_pg
+
+const EVEN_COLOR = "#7A7A7A";
+const ODD_COLOR = "#ADADAD";
+const SELECTION_COLOR = "#0169D9";
+
+var cytoscape = require("cytoscape");
 var jquery = require("jquery");
 var konva = require("konva");
 var edgeEditing = require("./cytoscape-edge-editing/src/index.js");
@@ -24,6 +24,117 @@ var contextMenus = require("cytoscape-context-menus");
 var undoRedo = require("cytoscape-undo-redo");
 var cola = require("cytoscape-cola");
 window.$ = jquery;
+undoRedo(cytoscape);
+contextMenus(cytoscape); // This line is crucial
+edgeEditing(cytoscape, jquery, konva);
+cytoscape.use(cola);
+
+let cy = cytoscape({
+  container: document.getElementById("cy"),
+  elements: [],
+  style: [
+    {
+      selector: "node",
+      style: {
+        width: "25",
+        height: "25",
+        content: "data(priority)",
+        "text-valign": "center",
+        "text-halign": "center",
+        color: "white",
+        "font-size": "10px",
+      },
+    },
+    {
+      selector: 'node[isEven = "true"]',
+      style: {
+        shape: "ellipse", // Round shape for even nodes
+        "background-color": EVEN_COLOR,
+      },
+    },
+    {
+      selector: 'node[isEven = "false"]',
+      style: {
+        shape: "rectangle", // Square shape for odd nodes
+        "background-color": ODD_COLOR,
+      },
+    },
+    {
+      selector: "node:selected",
+      style: {
+        "background-color": SELECTION_COLOR,
+      },
+    },
+    {
+      selector: "edge",
+      style: {
+        "curve-style": "bezier", // This makes the edge curved, which helps visually with arrow positioning
+        "target-arrow-shape": "triangle", // This creates a directed edge with an arrow pointing to the target node
+        //width: "1",
+        //"arrow-scale": "0.5",
+        //'target-arrow-color': '#000', // Optionally set the arrow color
+        //'line-color': '#000' // Optionally set the line color
+      },
+    },
+    {
+      selector: "edge:selected",
+      style: {
+        "line-color": SELECTION_COLOR, // Example selection color for edges
+        "target-arrow-color": SELECTION_COLOR, // Make sure the arrow matches the line
+        //'width': 4, // Optionally increase the width for visibility
+      },
+    },
+  ],
+});
+const cyContainer = cy.container();
+let copiedElements: cytoscape.ElementDefinition[] = [];
+const defaultLayout = "Force directed";
+
+cy.edgeEditing({
+  anchorShapeSizeFactor: 6,
+  enableMultipleAnchorRemovalOption: true,
+  enableCreateAnchorOnDrag: true,
+  zIndex: 0,
+  undoable: true,
+});
+
+cy.style().update();
+
+let ur = cy.undoRedo({
+  isDebug: true,
+});
+
+ur.action(
+  "editPriority",
+  (args) => {
+    let nodes = args.nodes;
+    let priority = args.priority;
+    // The do action: updating the priority
+    console.log(nodes);
+    console.log(priority);
+    let oldPriorities = nodes.map((node) => {
+      return { node: node, priority: node.data("priority") };
+    });
+    nodes.forEach(function (n) {
+      n.data("priority", priority);
+    });
+    return { nodes: nodes, oldPriorities: oldPriorities };
+  },
+  (args) => {
+    // The undo action: reverting to the old priorities
+    let oldPriorities = args.oldPriorities;
+    let newArgs = {
+      nodes: args.nodes,
+      priority: oldPriorities[0].node.data("priority"),
+    };
+    oldPriorities.forEach((item) => item.node.data("priority", item.priority));
+    return newArgs;
+  }
+);
+
+cy.add(pg.getElementDefinition());
+const layoutManager = new LayoutManager(cy, defaultLayout);
+layoutManager.runOnce();
 
 function resetSettings() {
   const layoutOnDrag = document.getElementById(
@@ -60,6 +171,8 @@ cy.on("afterDo", function (e, name) {
   console.log("afterDo", name);
 });
 
+let mouseX: number = 0;
+let mouseY: number = 0;
 
 function resetBoardVisuals() {
   const elements = pg.getElementDefinition();
@@ -147,6 +260,10 @@ function resetBoardVisuals() {
   }
 };
 
+cy.on("drag", "node", function () {
+  layoutManager.runLayout();
+});
+
 function updateGraphFileName(name: string) {
   const fileNameDisplay = document.getElementById("file-name-display");
   if (fileNameDisplay) {
@@ -168,8 +285,216 @@ function updateGraphFileName(name: string) {
   layoutManager.runOnce();
 };
 
+document.getElementById('layout-on-drag').addEventListener('change', function () {
+  layoutManager.toggleLayout((this as HTMLInputElement).checked);
+});
 
+document.getElementById('display-labels').addEventListener('change', function () {
+  const showLabels = (this as HTMLInputElement).checked;
+  cy.nodes().style({
+    'label': showLabels ? (ele: any) => `${ele.data("label")}\n${ele.data("priority")}` : '',
+    'text-wrap': 'wrap',
+  });
+});
 
+document.addEventListener("mousemove", (event: MouseEvent) => {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+});
+
+document.addEventListener("keydown", (event: KeyboardEvent) => {
+  const zoom = cy.zoom();
+  const pan = cy.pan();
+
+  // Convert the mouse coordinates to the model coordinates
+  const modelX = (mouseX - pan.x) / zoom;
+  const modelY = (mouseY - pan.y) / zoom;
+
+  switch (event.key) {
+    case "e": {
+      console.log("e pressed");
+      addNodeAtPosition(modelX, modelY, true);
+      break;
+    }
+    case "o": {
+      addNodeAtPosition(modelX, modelY, false);
+      break;
+    }
+    case "q": {
+      var selectedNodes = cy.$("node:selected");
+      selectedNodes.forEach((node) => {
+        let currentIsEven = node.data("isEven");
+        if (currentIsEven === "true") {
+          node.data("isEven", "false");
+        } else {
+          node.data("isEven", "true");
+        }
+      });
+      break;
+    }
+    case "Backspace":
+    case "Delete": {
+      var selectedElements = cy.$(":selected");
+
+      // Remove selected elements from the graph
+      if (selectedElements.length > 0) {
+        ur.do("remove", selectedElements);
+      }
+    }
+    case "+": {
+      var selectedNodes = cy.$("node:selected");
+      selectedNodes.forEach((node) => {
+        var priority = node.data("priority") || 0;
+        node.data("priority", priority + 1);
+      });
+      break;
+    }
+    case "-": {
+      var selectedNodes = cy.$("node:selected");
+      selectedNodes.forEach((node) => {
+        var priority = node.data("priority") || 0;
+        // Ensure priority does not go below 0
+        node.data("priority", Math.max(0, priority - 1));
+      });
+      break;
+    }
+    case "p": {
+      cy.elements().forEach(function (ele) {
+        console.log(ele.data());
+      });
+    }
+  }
+
+  if ((event.ctrlKey || event.metaKey) && event.key === "c") {
+    copySelectedElements();
+  }
+  // Paste with Ctrl+V or Cmd+V
+  if ((event.ctrlKey || event.metaKey) && event.key === "v") {
+    pasteCopiedElements();
+  }
+
+  if ((event.ctrlKey || event.metaKey) && event.key === "z") {
+    ur.undo();
+  }
+
+  if ((event.ctrlKey || event.metaKey) && event.key === "y") {
+    ur.redo();
+  }
+});
+
+function copySelectedElements() {
+  let selectedEles = cy.$(":selected").jsons();
+  // Deep copy and store in global variable
+  copiedElements = JSON.parse(JSON.stringify(selectedEles));
+}
+
+function pasteCopiedElements() {
+  if (copiedElements.length > 0) {
+    cy.$(":selected").unselect();
+    const offset = 10; // Offset for the pasted elements' position
+    let oldid_newid = {};
+    let maxId = getNewMaxId();
+
+    copiedElements.sort((a, b) => {
+      if (a.group === "nodes" && b.group === "edges") {
+        return -1;
+      }
+      if (a.group === "edges" && b.group === "nodes") {
+        return 1;
+      }
+      return 0;
+    });
+
+    let newElements = [];
+    copiedElements.map((ele) => {
+      if (ele.group === "nodes") {
+        oldid_newid[ele.data.id] = maxId;
+        ele.data.id = `${maxId}`; // Modify the ID to ensure uniqueness
+        ele.position.x += offset;
+        ele.position.y += offset;
+        maxId++;
+        newElements.push(ele);
+      } else if (
+        ele.group === "edges" &&
+        oldid_newid.hasOwnProperty(ele.data.source) &&
+        oldid_newid.hasOwnProperty(ele.data.target)
+      ) {
+        // Adjust source and target for edges to point to the new copied node IDs
+        ele.data.id = undefined; // Modify the ID to ensure uniqueness
+        ele.data.source = `${oldid_newid[ele.data.source]}`;
+        ele.data.target = `${oldid_newid[ele.data.target]}`;
+        newElements.push(ele);
+      }
+    }, []);
+
+    ur.do("add", newElements); // Add the new elements to the Cytoscape instance
+    copySelectedElements(); // Copy the newly pasted elements
+  }
+}
+
+function getNewMaxId() {
+  return (
+    cy.nodes().reduce((max, node) => {
+      const id = parseInt(node.data("id"));
+      return isNaN(id) ? max : Math.max(max, id);
+    }, 0) + 1
+  );
+}
+
+function addNodeAtPosition(x: number, y: number, isEven: boolean) {
+  ur.do("add", {
+    group: "nodes",
+    data: {
+      id: String(getNewMaxId()),
+      isEven: String(isEven), // Store isEven as a string to match the selector
+      priority: 1,
+    },
+    position: { x: x, y: y },
+  });
+  cy.resize();
+}
+
+cy.on("click", "node", (event) => {
+  const node = event.target;
+  const isAltPressed = event.originalEvent.altKey;
+  const isShiftCmdPressed =
+    event.originalEvent.shiftKey && event.originalEvent.metaKey;
+  if (!isAltPressed && !isShiftCmdPressed) return;
+
+  event.preventDefault();
+
+  // Get all currently selected nodes
+  const selectedNodes = cy.$("node:selected");
+
+  // Create an edge from each selected node to the shift-clicked node
+  selectedNodes.forEach((selectedNode) => {
+    const existingEdge = cy.edges().some((edge) => {
+      return (
+        edge.data("source") === selectedNode.id() &&
+        edge.data("target") === node.id()
+      );
+    });
+    if (!existingEdge) {
+      ur.do("add", {
+        group: "edges",
+        data: { source: selectedNode.id(), target: node.id() },
+      });
+    }
+  });
+
+  selectedNodes.unselect();
+});
+
+cyContainer.addEventListener(
+  "mousedown",
+  (event) => {
+    if (event.ctrlKey) {
+      event.preventDefault();
+      event.stopPropagation();
+    }
+  },
+  true
+);
 
 // Right click context menu
 cy.on("cxttap", "node", (event) => {
@@ -242,3 +567,4 @@ cy.on("cxttap", "node", function (event) {
     hideMenu();
   };
 });
+
