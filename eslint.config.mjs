11e43d0a486dc7ca69c6744ceaf66492839ffeda
Miki Vanoušek <vanousekmikulas@gmail.com>
2024-04-17 11:46:17 +0200

    add html linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,7 @@
 import globals from "globals";
 import tseslint from "typescript-eslint";
+import html from "@html-eslint/eslint-plugin";
+import parser from "@html-eslint/parser";
 
 import path from "path";
 import { fileURLToPath } from "url";
@@ -12,8 +14,23 @@ const __dirname = path.dirname(__filename);
 // const compat = new FlatCompat({baseDirectory: __dirname, recommendedConfig: pluginJs.configs.recommended});
 
 export default [
+
+  html.configs["flat/recommended"],
   { files: ["**/*.js"], languageOptions: { sourceType: "script" } },
   { languageOptions: { globals: globals.browser } },
   // ...compat.extends("standard-with-typescript"),
   ...tseslint.configs.recommended,
+  {
+    files: ["**/*.html"],
+    plugins: {
+      "@html-eslint": html,
+    },
+    languageOptions: {
+      parser,
+    },
+    rules: {
+      "@html-eslint/indent": "error",
+      "@html-eslint/indent": 2,
+    },
+  },
 ];
